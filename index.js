e98b6fbbfaab5d849e9a18d43047aafb847c5dd0
TanvirFahimBD <tanvirfahim6939@gmail.com>
2023-01-14 20:28:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//! middleware
+app.use(cors());
+app.use(express.json());
+
+//! server get
+app.get("/", (req, res) => {
+  res.send("Hello stock fruit Server");
+});
+
+//! server listen
+app.listen(port, () => {
+  console.log(`listening at port`, port);
+});
